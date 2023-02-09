44de311fc74dc4dc84f6f43861a45eca7bf33094
noir7bja@gmail.com <noir7bja@gmail.com>
2023-02-09 10:41:11 +0100

    JS00

---
+++ JS00/product.js
@@ -0,0 +1,4 @@
+var x= 5
+var y= 10
+var r= 15
+console.log(x*y*r)
---
+++ JS00/swapValues.js
@@ -0,0 +1,6 @@
+var a = 10
+var b = 20
+var a = 20
+var b = 10
+console.log("new value of a: " + a)
+console.log("new value of b: " + b)
